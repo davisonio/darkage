128f480e49bc85e3b6da7f9ec93022686c388335
adrido <robots_only_adrido@gmx.com>
2013-01-31 05:19:50 +0100

    add compatibility with old nodes from king arthurs land

---
+++ compatibility.lua
@@ -0,0 +1,10 @@
+--add compatibility with the old nodes of king arthurs land
+minetest.register_alias("darkage:stair_stonebrick", "darkage:stair_basalt_cobble")
+minetest.register_alias("darkage:stair_stonebrick_inverted", "darkage:stair_basalt_cobble_inverted")
+minetest.register_alias("darkage:stair_stonebrick_wall", "darkage:stair_basalt_cobble_wall")
+minetest.register_alias("darkage:slab_stonebrick_inverted", "darkage:slab_basalt_cobble_inverted")
+minetest.register_alias("darkage:slab_stonebrick_wall", "darkage:slab_basalt_cobble_wall")
+minetest.register_alias("darkage:slab_stonebrick", "darkage:slab_basalt_cobble")
+minetest.register_alias("darkage:panel_stonebrick_vertical", "darkage:panel_basalt_cobble_vertical")
+minetest.register_alias("darkage:panel_stonebrick_bottom", "darkage:panel_basalt_cobble_bottom")
+minetest.register_alias("darkage:panel_stonebrick_top", "darkage:panel_basalt_cobble_top")
--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ print (" ---- Dark Age is Loading! ---- ")
 dofile(minetest.get_modpath("darkage").."/mapgen.lua")
 dofile(minetest.get_modpath("darkage").."/building.lua")
 dofile(minetest.get_modpath("darkage").."/furniture.lua")
+dofile(minetest.get_modpath("darkage").."/compatibility.lua")
 --dofile(minetest.get_modpath("darkage").."/stairs.lua")
 dofile(minetest.get_modpath("darkage").."/stairsplus.lua")
 ----------
