faa6d36036311b6437c9eab1c55922d856f2dc54
CraigyDavi <craig.davison3@gmail.com>
2014-09-04 22:32:27 +0100

    Deprecated: minetest.env

--- mapgen.lua
+++ mapgen.lua
@@ -105,7 +105,7 @@ local function generate_stratus(name, wherein, ceilin, ceil, minp, maxp, seed, s
 				end
 				if x == 1 then
 					if ceil == nil then
-						minetest.env:set_node(p2, {name=name})
+						minetest.set_node(p2, {name=name})
 						i = i +1
 					else
 						local p3 = {p2.x,p2.y+1,p2}
@@ -201,8 +201,8 @@ local function generate_ore(name, wherein, minp, maxp, seed, chunks_per_volume, 
 							local y2 = y0+y1
 							local z2 = z0+z1
 							local p2 = {x=x2, y=y2, z=z2}
-							if minetest.env:get_node(p2).name == wherein then
-								minetest.env:set_node(p2, {name=name})
+							if minetest.get_node(p2).name == wherein then
+								minetest.set_node(p2, {name=name})
 							end
 						end
 					end
