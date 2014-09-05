8c0585b422e8a6df3531e3fc5ecfe080d2e2c38e
CraigyDavi <craig.davison3@gmail.com>
2014-09-05 16:16:56 +0100

    Add all the stairs! :smile:

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ dofile(minetest.get_modpath("darkage").."/nodes.lua")
 dofile(minetest.get_modpath("darkage").."/craftitems.lua")
 dofile(minetest.get_modpath("darkage").."/crafts.lua")
 dofile(minetest.get_modpath("darkage").."/mapgen.lua")
---dofile(minetest.get_modpath("darkage").."/stairs.lua") -- Currently broken, needs a rewrite ~CraigyDavi
+dofile(minetest.get_modpath("darkage").."/stairs.lua")
 dofile(minetest.get_modpath("darkage").."/aliases.lua")
 
 print ("Darkage [darkage] has loaded!")
